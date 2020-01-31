8069bbb59008a52c9c7ed8ec0c4c788ecd59874d
abiqna <60513656+abiqna@users.noreply.github.com>
2020-01-31 21:51:42 +0700

    Add files via upload

---
+++ ProgressBar.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progressbar;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author abi
+ */
+public class ProgressBar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ProgressBarDemo obj_frame = new ProgressBarDemo();
+        obj_frame.setTitle("Aplikasi Penentuan Kualitas Internet");
+        obj_frame.setResizable(false);
+        obj_frame.setVisible(true);
+    }
+    
+}
